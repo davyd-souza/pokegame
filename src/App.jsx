3bfc4e37501d0f4e07784270e00bbee70526b050
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-07-11 17:10:19 -0300

    Structured Pokemon cards; Updated icons; Added Pokemon API

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// Components
+import Pokecard from './components/Pokecard'
+
+// Style
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Pokecard />
+    </div>
+  );
+}
+
+//  App;
